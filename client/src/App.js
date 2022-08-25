ba9d4b8b9aeb03ab332038bd1a66fa0535f9126f
Ezequiel <ezequieldominguez11@gmail.com>
2022-08-25 10:01:09 -0300

    feat(src/utils/data.json): data loaded in json

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { Box } from "@chakra-ui/react"
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+
+      <Box color='gray.50' />
+
+      <Box color='#f00' />
+
+      <Box bg='tomato' w='100px' h='100px' />
+
+      <Box backgroundColor='tomato' />
     </div>
   );
 }
