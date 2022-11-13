3369b5f9272fb1280ba1eb78a80030ba203904fc
Cocahuana <ezequieldominguez11@gmail.com>
2022-11-13 13:13:28 -0300

    Desktop Hero create component

--- client/src/components/ImageOne.jsx
+++ client/src/components/ImageOne.jsx
@@ -1,51 +1,40 @@
-import {Parallax} from "react-parallax";
+import { Parallax } from "react-parallax";
 import Nasa from "../assets/img/background__home.jpg";
-import {Stack, Flex, Box, Text, Heading, Image} from "@chakra-ui/react";
-import {useContext} from "react";
-import {themeContext} from "../context/themeContext";
+import { Stack, Flex, Text, Heading, Box } from "@chakra-ui/react";
+import { useContext } from "react";
+import { themeContext } from "../context/themeContext";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+const title = "Soy una frase o titulo";
+const subtitle = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare nulla quam, 
+	bibendum pellentesque turpis porttitor ut. Etiam in risus augue. Pellentesque leo mi, commodo in lorem et, lacinia volutpat augue.`;
 function ImageOne() {
-	const myTheme = useContext(themeContext);
-	const {accent1} = myTheme;
 	return (
 		<Parallax
-			style={{heigth: "92vh", position: "relative"}}
+			style={{ heigth: "92vh", position: "relative" }}
 			bgImage={Nasa}
 			bgImageAlt='the cat'
-			strength={800}>
+			strength={800}
+		>
 			<Stack
 				height={"92vh"}
 				alignItems={"center"}
 				justifyContent='center'
-				width='100%'>
+				width='100%'
+			>
 				<Flex
 					color='white'
-					bg='rgba(0, 0, 0 ,0.78)'
+					bg='rgba(0, 0, 0 ,0.85)'
 					width='full'
-					h='full'>
+					h='full'
+					flexDirection='column'
+				>
 					<Box
-						flex='1'
-						display='flex'
-						flexDirection='column'
-						justifyContent='center'
-						alignItems='center'>
-						<Image src={Nasa} h='500px' w='500px' />
-					</Box>
-					<Box
-						flex='1'
-						display='flex'
-						flexDirection='column'
-						justifyContent='center'
-						alignItems='center'>
-						<Heading
-							as='h1'
-							size='4xl'
-							noOfLines={1}
-							color={accent1}>
-							Soy una frase o titulo
-						</Heading>
-						<Text>Box 3</Text>
-						<Text>Box 3</Text>
-						<Text>Box 3</Text>
+						display={{
+							base: "none",
+							lg: "flex",
+						}}
+					>
+						<DesktopHero />
 					</Box>
 				</Flex>
 			</Stack>
@@ -54,3 +43,39 @@ function ImageOne() {
 }
 
 export default ImageOne;
+
+const DesktopHero = () => {
+	const myTheme = useContext(themeContext);
+	const { accent1 } = myTheme;
+	return (
+		<>
+			<Flex position='absolute' right='15%' top='15%'>
+				<Heading as='h1' size='4xl' color={accent1} mb='10px'>
+					{title}
+				</Heading>
+			</Flex>
+			<Flex
+				position='absolute'
+				right='20%'
+				top='30%'
+				flexDirection='column'
+				maxW='30%'
+			>
+				<Text lineHeight={"30px"} fontSize='20px' textAlign={"justify"}>
+					{subtitle}
+				</Text>
+				<Text
+					mt='10px'
+					lineHeight={"30px"}
+					fontSize='20px'
+					letterSpacing='2px'
+					textAlign={"justify"}
+					fontWeight='800'
+					color={accent1}
+				>
+					Ver más <ArrowForwardIcon color='white' />{" "}
+				</Text>
+			</Flex>
+		</>
+	);
+};
